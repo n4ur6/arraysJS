1fcf331cf3fddfcc4814bd41dfb4797184bc158d
n4ur6 <naqeeburrehman783@gmail.com>
2024-12-16 00:44:13 +0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,78 @@
+// Arrays 
+// var fruits = ["Apple"  ,  "Banana"  , "Pinapple"  , "Mango"] ;
+// var cars = [" BMW"  ,  "Corolla"  ,  "Honda City"];
+ // console.log( fruits[0]  , fruits[1] , fruits[2] , fruits[3]);
+ // console.log( fruits.slince(0,3));
+// console.log( fruits.length);
+ // console.log(cars.pop());
+// console.log(cars.push("Fortuner"));
+//  console.log( cars[0]  ,  cars[1] , cars[2] );
+// console.log( myname ),
+//  var car = {
+ //    name  : 'limousine' , // key value pair 
+ //    model : '5000'  ,
+ //    color : 'white' ,
+ //    start : function start(){
+     // console.log( " i am starting a car");
+ //      alert(" starting a car");
+ //    }
+
+ // };
+
+ // console.log(car.name)
+// console.log( car.start());
+
+
+
+//  CONCEPTS
+//  console.log()
+
+ // var console = {
+ //  log : function bkjhfkshfjk() {
+  //  //   k jo bhi log k ander hai usy hmare jo browser ka console hai uss mai show krwa do
+ //     }
+ // }
+
+ // CONDITIONAL STATEMENTS
+ // age = 19 ;
+ // if (age>=18){
+ //    console.log("Eligible For ID card !");
+ // }
+ // else{
+ //   alert ("Sorry ! your are not eligble foe ID Card ");
+ // }
+ //  ________________________________
+ // IF _ ELSE _ IF 
+ // grade = 45;
+ // else if (grade < 100 && grade > 80 ) {
+ // alert ( " You have got A-Grade");
+ // }
+ // else if (grade < 80 && grade > 70 ) {
+ // alert ( " You have got B-Grade");
+ // }
+ // else if (grade < 70 && grade > 60 ) {
+ // alert ( " You have got C-Grade");
+ // }
+ // else if (grade < 60 && grade > 50 ) {
+ // alert ( " You have got D-Grade");
+ // }
+ // else{
+ // alert( "Sorry You are Fail. Try next Time ");
+ // ________________________________________________
+ // Password = "abc123";
+ // If(password === "abc123"){
+ //       console.log("LOGGED IN ")
+ // }
+ // ________________________________________________
+ // Scenario jo ap bana ry ho us pr depend krta hai
+ // num = 9;
+ // if( num % 2 === 0 ){
+ //   console.log("This is an even Number");
+ // }
+ //else {
+ // console.log("This is an odd Number ");
+ // }
+
+
+
+ 
